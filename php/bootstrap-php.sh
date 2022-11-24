d210ada81a810d7dbf29bb9c0bfaef0362bcce12
Brahim Machkouri <brahimmachkouri@users.noreply.github.com>
2022-11-24 17:17:07 +0100

    Added a readme

--- php/bootstrap-php.sh
+++ php/bootstrap-php.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# Create the folder
 mkdir -p /var/www/html
+
+# Create an empty CodeIgniter project
 /usr/bin/composer create-project CodeIgniter/framework /var/www/html
+
+# Since this is a dev tool, I don't care about the permissions
+# So the folder html is deletable externally
 chmod -R 777 /var/www/html
 chown -R www-data:www-data /var/www/html
 
-# This is the last line
+# !! This is the last line !!
 # If you don't run the process in the foreground, the container will exit with code 0
 php-fpm -F
---
+++ readme.md
@@ -0,0 +1,16 @@
+A docker-compose to build an Apache / PHP / PhpMyAdmin / Mariadb stack
+
+```
+docker-compose build --no-cache
+docker-compose up
+```
+
+### Welcome Page
+
+Connect to http://localhost
+
+### PhpMyAdmin
+
+Connect to http://localhost:8080
+
+An html folder is created, and all the app files are in this folder.
